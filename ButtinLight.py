1c13b4c668031d1d64c5124f4485dace1f43b6e2
Gaven Rudiak <58701821+AppleSpice@users.noreply.github.com>
2021-01-11 22:41:43 -0500

    Add files via upload

---
+++ ButtinLight.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+
+ledPin = 11
+buttonPin = 12
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ledPin, GPIO.OUT)
+    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO. PUD_UP)
+
+def loop():
+    while True:
+        if GPIO.input(buttonPin)==GPIO.LOW:
+            GPIO.output(ledPin,GPIO.HIGH)
+            print ('This just turned on')
+        else:
+            GPIO.output(ledPin, GPIO.LOW)
+            print ('led turned off')
+
+
+def destroy():
+    GPIO.cleanup()
+    print ('GPIO Pins cleared')
+
+if __name__ == '__main__':
+    print ('We starting')
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
